45ff334b5e0bfa1f59b00481e62cb8efaaa9be86
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-05-03 17:43:21 +0200

    learning ended

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 function App() {
   return ( 
-    <h1>Hello world!</h1> 
+    <h1 className='bg-[#a1a1a1] px-4 h-10 rounded gap-2 p-2 m-2 hover:bg-violet-800 transition-colors'>Hello world!</h1> 
   )
 }
 
