0088c6346bdc9b7f84feb2acd06f2d64e29e1efd
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-05-03 18:22:47 +0200

    Creating Widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import { useState } from 'react'
+import { Widget } from "./components/Widget";
 
-function App() {
-  return ( 
-    <h1 className='bg-[#a1a1a1] px-4 h-10 rounded gap-2 p-2 m-2 hover:bg-violet-800 transition-colors'>Hello world!</h1> 
-  )
+export function App() {
+  return <Widget/>
 }
-
-export default App
---
+++ src/components/Widget.tsx
@@ -0,0 +1,26 @@
+import { ChatTeardropDots } from 'phosphor-react'
+import { useState } from 'react'
+
+export function Widget() {
+
+    const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+    
+    function toggleWidgetVisibility(){
+        setIsWidgetOpen(!isWidgetOpen)
+    }
+
+    return (
+        <div className=' absolute bottom-5 right-5'>
+
+            {isWidgetOpen && <p>Hello world.</p> }
+
+            <button className=' bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group' onClick={ toggleWidgetVisibility }>
+                <ChatTeardropDots className=' w-6 h-6' />
+                <span className=' max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
+                    <span className='pl-2'></span>
+                    Feedback
+                </span>
+            </button>
+        </div>
+    )
+}
